26d4f19578c5e39c6a7229b6c941f64459d855f0
Andres <andresalba34@gmail.com>
2022-10-13 17:00:00 -0400

    create row of button to filter the products by category

--- lib/products/ui/screens/product_screen.dart
+++ lib/products/ui/screens/product_screen.dart
@@ -19,57 +19,98 @@ class ProductScreen extends StatelessWidget {
         body: Builder(
           builder: (ctx) {
             var productController = ctx.watch<ProductController>();
-            return GridView.count(
-              childAspectRatio: (1 / 1.5),
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              children: productController.allProduct
-                  .map(
-                    (x) => Card(
-                      elevation: 5,
-                      child: Container(
-                        height: 290,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20)),
-                        margin: const EdgeInsets.all(5),
-                        padding: const EdgeInsets.all(5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            SizedBox(
-                              height: 100,
-                              width: 100,
-                              child: Image.network(x.images.first),
+            return SingleChildScrollView(
+              child: Column(children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    CategoryFilterButton(name: 'Phones'),
+                    CategoryFilterButton(name: 'Laptops'),
+                    CategoryFilterButton(name: 'Fragrances'),
+                    CategoryFilterButton(name: 'SkinCare'),
+                    CategoryFilterButton(name: 'Groceries'),
+                    CategoryFilterButton(name: 'Home'),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                GridView.count(
+                  physics: const ScrollPhysics(),
+                  childAspectRatio: (1 / 1.5),
+                  crossAxisCount: 2,
+                  shrinkWrap: true,
+                  children: productController.allProduct
+                      .map(
+                        (x) => Card(
+                          elevation: 5,
+                          child: Container(
+                            height: 290,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20)),
+                            margin: const EdgeInsets.all(5),
+                            padding: const EdgeInsets.all(5),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                SizedBox(
+                                  height: 100,
+                                  width: 100,
+                                  child: Image.network(x.images.first),
+                                ),
+                                Text(
+                                  x.title,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
+                                      fontSize: 24),
+                                ),
+                                const SizedBox(height: 5),
+                                Text(
+                                  x.description,
+                                  maxLines: 2,
+                                  style: const TextStyle(fontSize: 12),
+                                ),
+                                Align(
+                                  alignment: Alignment.bottomRight,
+                                  child: Text(
+                                    x.stock.toString(),
+                                    textAlign: TextAlign.end,
+                                    style: const TextStyle(
+                                        fontSize: 12,
+                                        fontStyle: FontStyle.italic),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              x.title,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.w700, fontSize: 24),
-                            ),
-                            const SizedBox(height: 5),
-                            Text(
-                              x.description,
-                              maxLines: 2,
-                              style: const TextStyle(fontSize: 12),
-                            ),
-                            Align(
-                              alignment: Alignment.bottomRight,
-                              child: Text(
-                                x.stock.toString(),
-                                textAlign: TextAlign.end,
-                                style: const TextStyle(
-                                    fontSize: 12, fontStyle: FontStyle.italic),
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  )
-                  .toList(),
+                      )
+                      .toList(),
+                ),
+              ]),
             );
           },
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryFilterButton extends StatelessWidget {
+  final String name;
+  const CategoryFilterButton({Key? key, required this.name}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: SizedBox(
+        width: 65,
+        height: 40,
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            name,
+            style: const TextStyle(fontSize: 10),
+          ),
         ),
       ),
     );
